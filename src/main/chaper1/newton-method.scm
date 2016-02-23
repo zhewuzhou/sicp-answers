b50bcd5e5eeca5472bc252ad6b2b64b419c776b4
zhewuzhou <zhewuzhou@gmail.com>
2016-02-23 14:27:12 +0800

    [Z.Z][SCIP] different between if and procedure

--- src/main/chaper1/newton-method.scm
+++ src/main/chaper1/newton-method.scm
@@ -21,3 +21,19 @@
       guess
       (sqrt-iter (improve guess x)
                  x)))
+
+;;;1.6 what's wrong with flowing program, never return a value back
+;;;Lisp uses applicative-order evaluation, partly because of the additional efficiency obtained from avoiding multiple evaluations of expressions such as those illustrated with (+ 5 1) and (* 5 2) above and, more significantly, because normal-order evaluation becomes much more complicated to deal with when we leave the realm of procedures that can be modeled by substitution.
+;;;new-if is a procedure, not a special-form, which means that all sub-expressions are evaluated before new-if is applied to the values of the operands. That includes sqrt-iter which is extended to new-if which again leads to the evaluation of all the sub-expressions including sqrt-iter etc. Instead, in if only one of the consequent expressions is evaluated each time.
+
+(define (new-if predication then-clause else-clause)
+  (cond (predication (then-clause))
+        (else (else-clause))))
+
+(define (sqrt-iter-new guess x)
+  (new-if (good-enough? guess x)
+          guess
+          (sqrt-iter-new (improve guess x)
+                         x)))
+
+(sqrt-iter 1 2)
--- src/test/newton-method-test.scm
+++ src/test/newton-method-test.scm
@@ -1,9 +1,15 @@
 (load "../../test-manager/load.scm")
 (load "../main/chaper1/newton-method.scm")
 
-(define-test (sqrt-iter-2-with-guess-1)
+(define-test (test-sqrt-iter)
   (define result 0)
   (set! result (sqrt-iter 1 2))
   (check (< (abs (- (square result) 2)) 0.001) "should be good enough given 0.001"))
 
-(run-test '(sqrt-iter-2-with-guess-1))
+(define-test (test-sqrt-iter-new)
+  (define result 0)
+  (set! result (sqrt-iter-new 1 2))
+  (check (< (abs (- (square result) 2)) 0.001) "should be good with new-if"))
+
+(run-test '(test-sqrt-iter))
+;;;(run-test '(test-sqrt-iter-new))
