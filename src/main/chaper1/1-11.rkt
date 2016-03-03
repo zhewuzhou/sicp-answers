26f45bc97a8b81fb3377bf0696ee55b42c645b72
zhewuzhou <zhewuzhou@gmail.com>
2016-03-03 23:13:33 +0800

    [Z.Z][SCIP] implement recursive process for 1-11implement iterative process

--- src/main/chaper1/1-11.rkt
+++ src/main/chaper1/1-11.rkt
@@ -6,4 +6,16 @@
                  (* 2 (f (- n 2)))
                  (* 3 (f (- n 3)))))))
 
-(provide f)
+(define (g n)
+  (cond ((< n 3) n)
+        (else (iter-g 0 1 2 n))))
+
+(define (iter-g a b c count)
+  (if (= count 2)
+      c
+      (iter-g b c (+ c
+                     (* 2 b)
+                     (* 3 a)) (- count 1))))
+
+(provide f
+         g)
--- src/test/chaper1/1-11-test.rkt
+++ src/test/chaper1/1-11-test.rkt
@@ -4,12 +4,27 @@
   (require rackunit
            "../../main/chaper1/1-11.rkt")
   (test-case
-      "basic case"
+      "basic case for recursive process"
     (check-eq? (f 0) 0 "")
     (check-eq? (f 1) 1 "")
     (check-eq? (f 2) 2 ""))
+
   (test-case
-      "n bigger than 3"
+      "n bigger than 3 test for recursive process"
     (check-eq? (f 3) 4 "")
     (check-eq? (f 4) 11 "")
-    (check-eq? (f 5) 25 "")))
+    (check-eq? (f 5) 25 "")
+    (check-eq? (f 6) 59 ""))
+
+  (test-case
+      "basic case for iterative process"
+    (check-eq? (g 0) 0 "")
+    (check-eq? (g 1) 1 "")
+    (check-eq? (g 2) 2 ""))
+
+  (test-case
+      "n bigger than 3 test for iterative process"
+    (check-eq? (g 3) 4 "")
+    (check-eq? (g 4) 11 "")
+    (check-eq? (g 5) 25 "")
+    (check-eq? (g 6) 59 "")))
