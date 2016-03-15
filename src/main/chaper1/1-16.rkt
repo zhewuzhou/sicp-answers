fb54d2f191d6a56b6994612fd4400582b579b53f
zhewuzhou <zhewuzhou@gmail.com>
2016-03-15 13:04:04 +0800

    [Z.Z][SCIP] iterative process for b expt n

--- src/main/chaper1/1-16.rkt
+++ src/main/chaper1/1-16.rkt
@@ -6,9 +6,18 @@
 (define (even? n)
   (= (remainder n 2) 0))
 
+;;; This is the recursive process
 (define (fast-expt b n)
   (cond((= n 0) 1)
        ((even? n) (square (fast-expt b (/ n 2))))
        (else (* b (fast-expt b (- n 1))))))
 
-(provide fast-expt)
+;;; This is the iterative process
+
+(define (fast-expt-iter b n)
+  (cond ((= n 1) b)
+        ((even? n) (fast-expt-iter (square b) (/ n 2)))
+        (else (* b (fast-expt-iter (square b) (/ (- n 1) 2))))))
+
+(provide fast-expt
+         fast-expt-iter)
--- src/test/chaper1/1-16-test.rkt
+++ src/test/chaper1/1-16-test.rkt
@@ -5,4 +5,12 @@
            "../../main/chaper1/1-16.rkt")
   (test-case
       "fast-expt"
-    (check-eq? 32 (fast-expt 2 5) "")))
+    (check-eq? 32 (fast-expt 2 5) ""))
+  (test-case
+      "fast-expt-iter"
+    (check-eq? (fast-expt-iter 2 1) 2 "")
+    (check-eq? (fast-expt-iter 2 2) 4 "")
+    (check-eq? (fast-expt-iter 2 3) 8 "")
+    (check-eq? (fast-expt-iter 2 4) 16 "")
+    (check-eq? (fast-expt-iter 2 5) 32 "")
+    (check-eq? (fast-expt-iter 2 7) 128 "")))
