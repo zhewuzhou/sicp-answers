d1c8bd2e78161d659625089deccc155227a6bd6b
zhewuzhou <zhewuzhou@gmail.com>
2016-03-29 23:57:51 +0800

    [Z.Z][SICP] finish 1.32 with high order func

--- src/main/chaper1/1-31.rkt
+++ src/main/chaper1/1-31.rkt
@@ -9,6 +9,19 @@
         (iter (next n) (* (f n) result))))
   (iter a 1))
 
+(define (accumulate combiner null-value f a next b)
+  (define (iter n result)
+    (if (> n b)
+        result
+        (iter (next n) (combiner (f n) result))))
+  (iter a null-value))
+
+(define (mul a b)
+  (* a b))
+
+(define (product-acc f next a b)
+  (accumulate mul 1 f a next b))
+
 ;;; not a procedure means (n)
 (define (cal-pi n)
   (define (inc-2 x)
@@ -16,8 +29,8 @@
   (define (term x)
     (cond ((or (= x 2) (= x n)) x)
           (else (* x x))))
-  (* (/ (product term inc-2 2 n)
-        (product term inc-2 3 (- n 1)))
+  (* (/ (product-acc term inc-2 2 n)
+        (product-acc term inc-2 3 (- n 1)))
       4.0))
 
 (provide cal-pi)
