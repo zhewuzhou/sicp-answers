ca817ab7b785551b78844c3682ce2bab619c78b6
zhewuzhou <zhewuzhou@gmail.com>
2016-03-30 22:19:55 +0800

    [Z.Z][1.35] 1.61803278

--- src/test/chaper1/1-35-test.rkt
+++ src/test/chaper1/1-35-test.rkt
@@ -5,4 +5,8 @@
            "../../main/chaper1/1-35.rkt")
   (test-case
       "fixed-point"
-    (check-= (fixed-point cos 1.0) 0.73908229 1e-5)))
+    (check-= (fixed-point cos 1.0) 0.73908229 1e-5)
+    (check-= (fixed-point
+              (lambda (x)
+                (+ 1 (/ 1.0 x)))
+              1.0) 1.61803278 1e-5)))
