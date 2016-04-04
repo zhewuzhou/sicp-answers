99fc0e617550247a88f6c27d10cfcfbf35d18a63
zhewuzhou <zhewuzhou@gmail.com>
2016-04-04 20:58:58 +0800

    [Z.Z][1-45] using both newtons and damp finish this

--- src/main/chaper1/1-45.rkt
+++ src/main/chaper1/1-45.rkt
@@ -2,10 +2,19 @@
 
 (require "../utils/common.rkt"
          "./1-40.rkt"
-         "./1-41.rkt")
+         "./1-41.rkt"
+         "./1-35.rkt")
 
-(define (cube-root x)
-  (newtons-method (lambda(y)(- (* y y y) x))
+(define (nth-root-newtons x n)
+  (newtons-method (lambda(y)(- (expt y n) x))
                   1.0))
 
-(provide cube-root)
+(define (nth-root-damp x n)
+  (fixed-point
+   ((repeated average-damp
+              (floor (/ (log n) (log 2))))
+    (lambda(y)(/ x (expt y (- n 1)))))
+   1.0))
+
+(provide nth-root-newtons
+         nth-root-damp)
--- src/test/chaper1/1-45-test.rkt
+++ src/test/chaper1/1-45-test.rkt
@@ -5,5 +5,11 @@
            "../../main/chaper1/1-45.rkt"
            "../../main/chaper1/1-40.rkt")
   (test-case
-      "cube-root"
-    (check-= (cube-root 8.0) 2.0 1e-5 "")))
+      "nth-root-newtons"
+    (check-= (nth-root-newtons 10000 4) 10 1e-5 "")
+    (check-= (nth-root-newtons 4294967296 32) 2.0 1e-5 ""))
+  (test-case
+      "nth-root-damp"
+    (check-= (nth-root-damp 10000 4) 10 1e-5 "")
+    (check-= (nth-root-damp 4294967296 32) 2.0 1e-5 "")))
+
