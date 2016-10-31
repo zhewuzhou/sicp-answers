7e7f96a6ac4fb2ed8786447e25470227f0610be2
zhewuzhou <zhewuzhou@gmail.com>
2016-10-31 17:35:09 +0800

    [Z.Z][SICP] 2-4-6

---
+++ src/main/chaper2/2-4-6.rkt
@@ -0,0 +1,35 @@
+#lang racket/base
+
+(require "../chaper1/1-16.rkt")
+
+(define (cons x y)
+  (λ (m)
+     (m x y)))
+
+(define (car z)
+  (z (λ (p q)
+        p)))
+
+(define (cdr z)
+  (z (λ (p q)
+        q)))
+
+(define (cons-expt a b)
+  (* (fast-expt 2 a)
+     (fast-expt 3 b)))
+
+(define (expt-internal n b s)
+  (cond
+   ((= (modulo n b) 0)
+    (expt-internal (/ n b)
+                   b
+                   (+ s 1)))
+   (else s)))
+
+(define (car-expt n)
+  (expt-internal n 2 0))
+
+(define (cdr-expt n)
+  (expt-internal n 3 0))
+
+(provide cons car cdr cons-expt car-expt cdr-expt)
---
+++ src/test/chaper2/2-4-6-test.rkt
@@ -0,0 +1,15 @@
+#lang racket/base
+
+(module+ test
+         (require rackunit "../../main/chaper2/2-4-6.rkt")
+         (test-case "test cons car cdr"
+                    (check-eq? (car (cons 2 1))
+                               2)
+                    (check-eq? (cdr (cons 2 1))
+                               1))
+         (test-case "test cons car cdr with expt"
+                    (define large-number 2519424)
+                    (check-eq? (car-expt large-number)
+                               7)
+                    (check-eq? (cdr-expt large-number)
+                               9)))
